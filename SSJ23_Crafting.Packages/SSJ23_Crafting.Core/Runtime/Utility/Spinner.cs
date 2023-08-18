9382cb7be4404a5f3f4a1fce82134110634e6994
Jacob Penner <jkpenner@live.com>
2023-08-18 16:32:57 -0700

    Update scene with battlefield and player platforms

---
+++ SSJ23_Crafting.Packages/SSJ23_Crafting.Core/Runtime/Utility/Spinner.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace SSJ23_Crafting
+{
+    public class Spinner : MonoBehaviour
+    {
+        public float speed = 1f;
+        public Vector3 axis = Vector3.up;
+        public Space space = Space.Self;
+
+
+        public void Update()
+        {
+            transform.Rotate(axis * speed * Time.deltaTime, space);
+        }
+
+    }
+}
