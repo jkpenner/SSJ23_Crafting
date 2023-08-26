e23a5bb7ffe0a13164fe4d58932a1f0c251783a8
jkpenner <jkpenner@live.com>
2023-08-26 11:33:35 -0700

    Implement Spawn Object Card behaviour

--- SSJ23_Crafting.Packages/SSJ23_Crafting.Core/Runtime/Cards/Types/Attachments/Damagers/SpawnObjectCard.cs
+++ SSJ23_Crafting.Packages/SSJ23_Crafting.Core/Runtime/Cards/Types/Attachments/Damagers/SpawnObjectCard.cs
@@ -9,5 +9,33 @@ namespace SSJ23_Crafting
         [SerializeField] Vector3 offset;
         [SerializeField] float spawnInterval;
         [SerializeField] bool spawnWhileGrounded;
+
+        private float counter = 0f;
+
+        public override void OnCardUpdate()
+        {
+            counter += Time.deltaTime;
+            if (counter < spawnInterval)
+            {
+                return;
+            }
+
+            if (spawnWhileGrounded && !Owner.Motor.IsGrounded)
+            {
+                return;
+            }
+
+            counter = 0f;
+            var gameObject = GameObject.Instantiate(
+                prefab, 
+                Owner.transform.position + offset, 
+                Owner.transform.rotation
+            );
+
+            foreach(var hasOwner in gameObject.GetComponents<HasOwner>())
+            {
+                hasOwner.Owner = Owner;
+            }
+        }
     }
 }
